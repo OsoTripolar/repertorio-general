1a096856d47af11b64e112d8ee382ce9274fd0fd
unknown <omar.trafalgar.2001@gmail.com>
2024-08-20 02:42:25 -0500

    se agregó la funcionalidad james

--- js/script.js
+++ js/script.js
@@ -17,10 +17,29 @@ document.getElementById('activador').addEventListener('input', function() {
 });
 
 document.addEventListener('keydown', function(event) {
+
     // Verifica si la tecla presionada es la "l" (minúscula)
     if (event.key === 'l') {
+
         event.preventDefault(); // Evita que la "l" se escriba en el input
+
         // Redirige al formulario en el footer
         document.getElementById('activador').focus(); // Lleva el foco al campo de entrada del formulario
     }
+});
+
+document.getElementById('boton').addEventListener('click', function() {
+    // Selecciona todos los elementos con las clases repertorio__artista y repertorio__cancion
+    const elementos = document.querySelectorAll('.repertorio__artista, .repertorio__cancion');
+
+    // Oculta todos los elementos seleccionados
+    elementos.forEach(elemento => {
+        elemento.style.display = 'none';
+    });
+
+    // Selecciona todos los elementos que también tienen la clase .james y los muestra
+    const jamesElementos = document.querySelectorAll('.james');
+    jamesElementos.forEach(elemento => {
+        elemento.style.display = 'block';
+    });
 });
